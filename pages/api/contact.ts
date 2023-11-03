3fcf0b60b17839bdaeeec716b93943bafb8b0505
thooombs <thomaz639@gmail.com>
2023-11-02 22:56:46 -0300

    espaco fix

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,17 +2,19 @@ import { Resend } from "resend";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { EmailTemplate } from "@/app/components/email-template";
 
-const resend = new Resend('re_5hqut7DW_Myci3pCv4hFBSXqUvnGxkx3M');
+const resend = new Resend("re_5hqut7DW_Myci3pCv4hFBSXqUvnGxkx3M");
 export default async (req: NextApiRequest, res: NextApiResponse) => {
- 
-  
-  try{
+  try {
     const data = await resend.emails.send({
-      from: 'FABE LP <onboarding@resend.dev>',
+      from: "FABE LP <onboarding@resend.dev>",
       to: "thomaz639@gmail.com",
       subject: `${req.body.nome} - Nova Inscrição FABE`,
-      html: `${req.body.nome} <br/> ${req.body.fone} <br/>  ${req.body.email} <br/>  ${req.body.cpf} <br/> ${req.body.curso}`,
-    })
+      html: `nome: ${req.body.nome} <br/>
+      fone:  ${req.body.fone} <br/>
+      email:  ${req.body.email} <br/>
+      cpf:   ${req.body.cpf} <br/>
+      curso:  ${req.body.curso}`,
+    });
 
     res.status(200).json(data);
   } catch (error) {
