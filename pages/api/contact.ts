1fbfc93fd010d92aca2b9d28588f2758b326993d
thooombs <thomaz639@gmail.com>
2023-11-02 22:43:03 -0300

    html fix

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -8,10 +8,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   
   try{
     const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
+      from: 'FABE LP <onboarding@resend.dev>',
       to: "thomaz639@gmail.com",
-      subject: `${req.body.nome} FABE 2024`,
-      html: `${req.body.nome} ${req.body.fone} ${req.body.email} ${req.body.cpf} ${req.body.curso}`,
+      subject: `${req.body.nome} - Nova Inscrição FABE`,
+      html: `${req.body.nome} </br> ${req.body.fone} </br>  ${req.body.email} </br>  ${req.body.cpf} </br> ${req.body.curso}`,
     })
 
     res.status(200).json(data);
