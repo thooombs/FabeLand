643cb493458e456be3abfc9430b90d9249928933
thooombs <thomaz639@gmail.com>
2023-11-17 21:10:49 -0300

    BRUNA

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,12 +2,13 @@ import { Resend } from "resend";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { EmailTemplate } from "@/app/components/email-template";
 
+
 const resend = new Resend("re_5hqut7DW_Myci3pCv4hFBSXqUvnGxkx3M");
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const data = await resend.emails.send({
       from: "FABE LP <onboarding@resend.dev>",
-      to: "thomaz639@gmail.com",
+      to: "maronibruuna@gmail.com",
       subject: `${req.body.nome} - Nova Inscrição FABE`,
       html: `nome: ${req.body.nome} <br/>
       
